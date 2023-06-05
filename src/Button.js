18090a3a5434788e0a95b6ef1a50a08a57642df7
SajanG <sajantamu04@gmail.com>
2023-06-05 20:56:33 +1000

    added button

---
+++ src/Button.js
@@ -0,0 +1,12 @@
+import { Button } from "bootstrap";
+import { Component } from "react";
+
+class Button extends Component {
+  render() {
+    <>
+      <Button className="btn btn-primary">React</Button>
+    </>;
+  }
+}
+
+export default Button;

05437df70940d4331dd64c97613a1c4943ddbb35
SajanG <sajantamu04@gmail.com>
2023-06-05 21:16:52 +1000

    deleted button

--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Button2 from "./Button";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-    <Button2 />
   </React.StrictMode>
 );
